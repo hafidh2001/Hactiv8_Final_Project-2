43d95cfb2355009eed166ed19365d6382c2ca363
hafidh2001 <hafidhfauzan2@gmail.com>
2022-11-07 13:56:03 +0700

    fixing

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(express.json());
 app.use(routes);
 
 // declare route
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log(`🍃 Server running at http://localhost:${port}/`);
 });
