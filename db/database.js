96a67f130b80ba567eea756f7a03914ea62a5d72
hafidh2001 <hafidhfauzan2@gmail.com>
2022-11-09 21:52:18 +0700

    fixing

--- db/database.js
+++ db/database.js
@@ -3,20 +3,21 @@ import { Sequelize } from "sequelize";
 
 const db =
   node_env === "production"
-    ? new Sequelize(database_url, {
-        dialect: "postgres",
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
-          },
-        },
-        typeValidation: true,
-      })
+    ? new Sequelize(database_url)
     : new Sequelize(database.name, database.user, database.password, {
         host: database.host,
         dialect: "postgres",
         typeValidation: true,
       });
 
+// {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+//     },
+//   },
+//   typeValidation: true,
+// }
 export default db;
